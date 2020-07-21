bef22b0893db0ab00e185adb603860d730373e5c
Ashwani Singal <singalashwani@rediffmail.com>
2020-07-21 16:15:53 +0530

    Updates

---
+++ precache-manifest.c09483671e4c45c1634342cd8f297103.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd053e80c5d6c7c32a73b08cb52b4f6",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "74e77df4cadaa6ed66e0",
+    "url": "/reactwebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "a10ef0c624c6c9c9849a",
+    "url": "/reactwebsite/static/css/main.35995386.chunk.css"
+  },
+  {
+    "revision": "74e77df4cadaa6ed66e0",
+    "url": "/reactwebsite/static/js/2.f065fa69.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite/static/js/2.f065fa69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10ef0c624c6c9c9849a",
+    "url": "/reactwebsite/static/js/main.be5e5b4e.chunk.js"
+  },
+  {
+    "revision": "0bb169a3a757acaebf1e",
+    "url": "/reactwebsite/static/js/runtime-main.d6619741.js"
+  },
+  {
+    "revision": "7e54a2d3f1c6317eba97896843cc1460",
+    "url": "/reactwebsite/static/media/cough.7e54a2d3.png"
+  },
+  {
+    "revision": "bedff6cea3b818b20a06c88fc45c3e30",
+    "url": "/reactwebsite/static/media/doctor.bedff6ce.png"
+  },
+  {
+    "revision": "fc0cdb848ffb8c4494e62baf187ab514",
+    "url": "/reactwebsite/static/media/fever.fc0cdb84.jpg"
+  },
+  {
+    "revision": "1e14d19daacd851098f1d3823a2374fb",
+    "url": "/reactwebsite/static/media/home.1e14d19d.jpg"
+  },
+  {
+    "revision": "cef446f61ed3c78bd491f6f53c2634d7",
+    "url": "/reactwebsite/static/media/isolate.cef446f6.jpg"
+  },
+  {
+    "revision": "4eeebd42b4a15af4a3af8c07b4537492",
+    "url": "/reactwebsite/static/media/news.4eeebd42.jpg"
+  },
+  {
+    "revision": "ecab7c93cac7553b7cea89ad4c3a18c4",
+    "url": "/reactwebsite/static/media/tirdness.ecab7c93.jpg"
+  }
+]);
